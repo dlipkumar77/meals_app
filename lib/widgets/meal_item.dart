a7f87b1d7c5f2d2d95afc70f1c87ada997503f8b
deepak <dlipkumar77@gmail.com>
2025-01-30 13:03:57 +0530

    Meals page sub text area improving

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 import '/models/meal.dart';
 
@@ -6,6 +7,16 @@ class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +61,26 @@ class MealItem extends StatelessWidget {
                       height: 12,
                     ),
                     Row(
-                      children: [],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                            icon: Icons.schedule,
+                            label: '${meal.duration} min'),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: complexityText,
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.attach_money,
+                          label: affordabilityText,
+                        ),
+                      ],
                     )
                   ],
                 ),
